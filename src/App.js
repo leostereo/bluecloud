23eb22eec8fb95d1a81643d215ac7eea2b848937
leo <ingrogger@gmail.com>
2023-08-18 08:31:07 -0300

    firebase

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  function onButtonClick() {
+
+
+  
+    let options = {};
+    options.acceptAllDevices = true;
+
+  
+    console.log('Requesting Bluetooth Device...');
+    console.log('with ' + JSON.stringify(options));
+    navigator.bluetooth.requestDevice(options)
+    .then(device => {
+      console.log('> Name:             ' + device.name);
+      console.log('> Id:               ' + device.id);
+      console.log('> Connected:        ' + device.gatt.connected);
+    })
+    .catch(error => {
+      console.log('Argh! ' + error);
+    });
+  }
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reloadddd
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={onButtonClick}>BLE</button>
       </header>
     </div>
   );
