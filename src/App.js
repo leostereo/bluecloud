63108ac130252c73017ddc842f5996ef01f8c9d2
leo <ingrogger@gmail.com>
2023-08-19 09:45:46 -0300

    basic test

--- src/App.js
+++ src/App.js
@@ -2,54 +2,57 @@ import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class GenericDevice {
-
-  constructor() {
-    this.device = null;
-    this.onDisconnected = this.onDisconnected.bind(this);
-  }
-  
-  async request() {
-
-    let options = {
-      "filters": [{
-        "name": "blehr_sensor_1.0"
-      }],
-      "optionalServices": [0xDDDD]
-    };
-    this.device = await navigator.bluetooth.requestDevice(options);
-    if (!this.device) {
-      throw "No device selected";
-    }
-    this.device.addEventListener('gattserverdisconnected', this.onDisconnected);
-  }
-  
-  async connect() {
-    if (!this.device) {
-      return Promise.reject('Device is not connected.');
-    }
-    await this.device.gatt.connect();
-  }
-  
-  async readManufacturername() {
-    const service = await this.device.gatt.getPrimaryService(0xDDDD);
-    const characteristic = await service.getCharacteristic(0xFFFF);
-    await characteristic.readValue();
-    alert(characteristic)
-  }
-
-  disconnect() {
-    if (!this.device) {
-      return Promise.reject('Device is not connected.');
-    }
-    return this.device.gatt.disconnect();
-  }
-
-  onDisconnected() {
-    console.log('Device is disconnected.');
-  }
-}
 function App() {
+  class GenericDevice {
+  
+    constructor() {
+      this.device = null;
+      this.onDisconnected = this.onDisconnected.bind(this);
+    }
+    
+    async request() {
+  
+      let options = {
+        "filters": [{
+          "name": "blehr_sensor_1.0"
+        }],
+        "optionalServices": [0xDDDD]
+      };
+      try{
+  
+        this.device = await navigator.bluetooth.requestDevice(options);
+      }catch{
+  
+        alert("No device selected");
+      }
+      this.device.addEventListener('gattserverdisconnected', this.onDisconnected);
+    }
+    
+    async connect() {
+      if (!this.device) {
+        return Promise.reject('Device is not connected.');
+      }
+      await this.device.gatt.connect();
+    }
+    
+    async readManufacturername() {
+      const service = await this.device.gatt.getPrimaryService(0xDDDD);
+      const characteristic = await service.getCharacteristic(0xFFFF);
+      await characteristic.readValue();
+      setData(characteristic);
+    }
+  
+    disconnect() {
+      if (!this.device) {
+        return Promise.reject('Device is not connected.');
+      }
+      return this.device.gatt.disconnect();
+    }
+  
+    onDisconnected() {
+      console.log('Device is disconnected.');
+    }
+  }
 
   const [data, setData] = useState({});
 
